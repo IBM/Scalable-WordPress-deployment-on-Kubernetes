0623b5da9360a245be3885006056cc91fb9a29e9
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-03-17 15:45:30 -0700

    updated jarvis

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ curl -L "https://cli.run.pivotal.io/stable?release=macosx64-binary&source=github
 mv cf /usr/local/bin
 cf --version
 curl "http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.0_amd64.tar.gz" | tar -zxv
-./Bluemix_CLI/install_bluemix_cli
+sudo ./Bluemix_CLI/install_bluemix_cli
 bx login
 bx plugin repo-add Bluemix https://plugins.ng.bluemix.net
 bx plugin install container-service -r Bluemix
