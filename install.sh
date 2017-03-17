ab07b21071b36d574503c2f53f092d5b57d3c65a
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-03-17 16:35:09 -0700

    jarvas encrypt

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mv cf /usr/local/bin
 cf --version
 curl "http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.0_amd64.tar.gz" | tar -zxv
 sudo ./Bluemix_CLI/install_bluemix_cli
-bx login
+bx login 
 bx plugin repo-add Bluemix https://plugins.ng.bluemix.net
 bx plugin install container-service -r Bluemix
 bx cs init
