d0e5ee5e17b87d9c703c9ad6475e8b793090f061
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-04-12 11:30:59 -0700

    added sleep when removing deployments

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -3,7 +3,12 @@ set -x
 kubectl delete --ignore-not-found=true -f local-volumes.yaml
 kubectl delete --ignore-not-found=true secret mysql-pass
 kubectl delete --ignore-not-found=true svc,pvc,deployment -l app=wordpress
-sleep 120s
+
+kuber=$(kubectl get pods -l app=wordpress)
+if [ ${#kuber} -ne 0 ]; then
+	sleep 120s
+fi
+
 echo 'password' > password.txt
 tr -d '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
 kubectl create -f local-volumes.yaml
